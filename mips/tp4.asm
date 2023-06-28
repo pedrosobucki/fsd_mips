3baef15ff92780076bec9fd03a8300f74790aa6c
pedrosobucki <pedrosobucki@gmail.com>
2023-06-28 15:52:12 -0300

    fixed missing lw's

--- mips/tp4.asm
+++ mips/tp4.asm
@@ -14,9 +14,12 @@ main:  la     $s0, n
 
        add    $t3, $0, $0   # i = 0
 
-sum:   add    $s2, $s2, 0($s5)     # $s2 += a[i] 
-       add    $s3, $s3, 0($s6)     # $s3 += b[i]
-       add    $s4, $s4, 0($s7)     # $s4 += c[i]
+sum:   lw     $t0, 0($s5)          # $t0 = a[i]
+       add    $s2, $s2, $t0        # $s2 += a[i] 
+       lw     $t0, 0($s6)          # $t0 = b[i]
+       add    $s3, $s3, $t0        # $s3 += b[i]
+       lw     $t0, 0($s7)          # $t0 = c[i]
+       add    $s4, $s4, $t0        # $s4 += c[i]
        addi   $t3, $t3, 1          # i++
        addi   $s5, $s5, 4          # pega próximo de A
        addi   $s6, $s6, 4          # pega próximo de B
@@ -41,7 +44,7 @@ media: add    $s1, $s2, $0  # $s1 = sum_a
        bgt    $s3, $s1, min_c      # if sum_b > min then skip B
        add    $s1, $s3, $0         # else, min = sum_b
 
-min_c: bgt    $s4, $s1, prepare_d     # if sum_c > min then skip C
+min_c: bgt    $s4, $s1, prepare_d  # if sum_c > min then skip C
        add    $s1, $s4, $0         # else, min = sum_c
 
 
@@ -50,26 +53,29 @@ prepare_d:    la     $s2, D        # $s2 = D
               add    $t3, $0, $0   # $t3 = i = 0
 
 
-sv_a:  bgt    0($s5), $s1, sv_b
-       add    0($s2), 0($s5), 0($s5)
-       addi   $s3, $s3, 1
-       addi   $s2, $s2, 4
+sv_a:  lw     $t0, 0($s5)          # $t0 = a[i]
+       bgt    $t0, $s1, sv_b       # if a[i] > min then goto sv_b
+       sw     $t0, 0($s2)          # d[k] = a[i]
+       addi   $s3, $s3, 1          # k++     
+       addi   $s2, $s2, 4          # próximo valor de d
 
-sv_b:  bgt    0($s6), $s1, sv_c
-       add    0($s2), 0($s6), 0($s6)
-       addi   $s3, $s3, 1
-       addi   $s2, $s2, 4
+sv_b:  lw     $t0, 0($s6)          # $t0 = b[i]
+       bgt    $t0, $s1, sv_c       # if b[i] > min then goto sv_c
+       sw     $t0, 0($s2)          # d[k] = b[i]
+       addi   $s3, $s3, 1          # k++     
+       addi   $s2, $s2, 4          # próximo valor de d
 
-sv_c:  bgt    0($s7), $s1, cond
-       add    0($s2), 0($s7), 0($s7)
-       addi   $s3, $s3, 1
-       addi   $s2, $s2, 4
+sv_c:  lw     $t0, 0($s7)          # $t0 = c[i]
+       bgt    $t0, $s1, cond       # if c[i] > min then goto cond
+       sw     $t0, 0($s2)          # d[k] = c[i]
+       addi   $s3, $s3, 1          # k++     
+       addi   $s2, $s2, 4          # próximo valor de d
 
-cond:  addi   $s5, $s5, 4
-       addi   $s6, $s6, 4
-       addi   $s7, $s7, 4
-       addi   $t3, $t3, 1
-       blt    $t3, $s0, sv_a
+cond:  addi   $s5, $s5, 4          # próximo valor de a
+       addi   $s6, $s6, 4          # proximo valor de b
+       addi   $s7, $s7, 4          # proximo valor de c
+       addi   $t3, $t3, 1          # i++
+       blt    $t3, $s0, sv_a       # if i < k then goto sv_a
 
 
 st:    la     $t0, k
